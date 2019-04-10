e974196e62084c7082b13a02909c09799b478cc0
Noah <noah.stoeckel@gmail.com>
2019-04-10 16:24:06 -0400

    Implements Fails Test Suite

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -55,4 +55,45 @@ describe("enhancer test suite", () => {
       expect(actual).toEqual(expected);
     });
   });
+  describe("fail test suite", () => {
+    it("should decrease the durability of an item by 5 if the enhancement is less than 15", () => {
+      // arrange
+      const item = { name: "broadsword", enhancement: 14, durability: 80 };
+      const expected = {
+        name: "broadsword",
+        enhancement: 14,
+        durability: 75
+      };
+      // act
+      const actual = enhancer.fail(item);
+      // assert
+      expect(actual).toEqual(expected);
+    });
+    it("should decrease the durability of the item by 10 if the enhancement is 15 or more", () => {
+      // arrange
+      const item = { name: "broadsword", enhancement: 16, durability: 80 };
+      const expected = {
+        name: "broadsword",
+        enhancement: 16,
+        durability: 70
+      };
+      // act
+      const actual = enhancer.fail(item);
+      // assert
+      expect(actual).toEqual(expected);
+    });
+    it("decreases the enhancement level by one if the enhancement level is greater than 16 and durability by 10", () => {
+      // arrange
+      const item = { name: "broadsword", enhancement: 17, durability: 80 };
+      const expected = {
+        name: "broadsword",
+        enhancement: 16,
+        durability: 70
+      };
+      // act
+      const actual = enhancer.fail(item);
+      // assert
+      expect(actual).toEqual(expected);
+    });
+  });
 });
