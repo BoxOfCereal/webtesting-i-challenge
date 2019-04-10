0f54ffc8eb09383a15a367c17dbfacbcfbf86f2c
Noah <noah.stoeckel@gmail.com>
2019-04-09 23:49:39 -0400

    Tests The Repair Function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,9 +1,30 @@
 const enhancer = require("./enhancer.js");
 // test away!
 describe("enhancer test suite", () => {
-  it("should repair the item", () => {
-    // arrange
-    // act
-    // assert
+  describe("repair test suite", () => {
+    it("should repair the durability of the item to 100 if less than 100", () => {
+      // arrange
+      const item = { name: "broadsword", enhancement: "0", durability: 80 };
+      const expected = {
+        name: "broadsword",
+        enhancement: "0",
+        durability: 100
+      };
+
+      // act
+      const actual = enhancer.repair(item);
+      // assert
+      expect(actual).toEqual(expected);
+    });
+    it("should keep the durability of the item if it is 100", () => {
+      // arrange
+      const item = { name: "dagger", enhancement: "1", durability: 100 };
+      const expected = { name: "dagger", enhancement: "1", durability: 100 };
+
+      // act
+      const actual = enhancer.repair(item);
+      // assert
+      expect(actual).toEqual(expected);
+    });
   });
 });
